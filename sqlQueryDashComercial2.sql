aa9a1488465e23a18244e3c89e4762e04f35bd9a
Luiz Doná <86530779+LuizFDona@users.noreply.github.com>
2023-01-27 15:07:35 -0300

    Update sqlQueryDashComercial2.sql

--- sqlQueryDashComercial2.sql
+++ sqlQueryDashComercial2.sql
@@ -1,7 +1,9 @@
  -- VIEW TABELA dClientes 
  -- Realizado Join na tabela de geografia para pegar informações de localidade
- 
+ DROP VIEW VwdCliente
+
  CREATE VIEW VwdCliente AS
+
 
    SELECT 
     dCliente.CustomerKey AS 'IdCliente',
@@ -13,6 +15,7 @@
 	dCliente.YearlyIncome AS 'RendaAnual',
 	dCliente.TotalChildren AS 'QtdFilhos',
 	dCliente.Occupation AS 'Ocupacao',
+	dCliente.DateFirstPurchase AS 'PrimeiraCompra',
 	DATEDIFF(YEAR, dCliente.BirthDate, GETDATE()) AS 'Idade',
 		CASE
 			WHEN DATEDIFF(YEAR,dCliente.BirthDate, GETDATE()) > 70 THEN 'Acima de 70 anos'
@@ -23,13 +26,35 @@
 		END AS 'CategoriaIdade',
 	dGeografia.ContinentName  AS 'Continente',
 	dGeografia.RegionCountryName AS 'Pais',
-	dGeografia.StateProvinceName AS 'Estado',
-	dGeografia.CityName AS 'Cidade'
-
+	CONCAT(dGeografia.StateProvinceName, ', ', dGeografia.RegionCountryName) AS 'Estado',
+	CONCAT(dGeografia.CityName, ', ', dGeografia.StateProvinceName, ', ', dGeografia.RegionCountryName) AS 'Cidade',
+	dGeografia.StateProvinceName AS 'Estado-Provincia',
+	dGeografia.CityName AS 'Municipio'
+	
   FROM [dbo].[DimCustomer] AS dCliente
   LEFT JOIN DimGeography AS dGeografia ON dCliente.GeographyKey = dGeografia.GeographyKey
   WHERE CONCAT(dCliente.FirstName, ' ', dCliente.MiddleName, ' ', dCliente.LastName) <> ''
 
+-- Criar tabela URLs 
+-- Tabela utilizada para incluir foto de cada categoria na tabela dProdutos
+-- Os links são URLs do google Drive contendo a respectiva foto de cada categoria
+
+
+CREATE TABLE dImagem (
+	[IdCategoria] int,
+	[Url] varchar(800)
+	)
+
+INSERT INTO dImagem 
+	([IdCategoria], [Url])
+VALUES(1, 'link1'),
+(2, 'link2'),
+(3, 'link3'),
+(4, 'link3'),
+(5, 'link4'),
+(6, 'link5'),
+(7, 'link6'),
+(8, 'link7')
 
 
 -- VIEW TABELA dLoja
@@ -47,26 +72,9 @@ WHERE dLoja.StoreName IN (
 						)
 
 -- Criar tabela URLs 
--- Tabela utilizada para incluir foto de cada categoria na tabela dProdutos
 
-CREATE TABLE dImagem (
-	[IdCategoria] int,
-	[Url] varchar(800)
-	)
-
-INSERT INTO dImagem 
-	([IdCategoria], [Url])
-VALUES(1, 'https://drive.google.com/uc?id=1DczZ1prOhx33FSZpukxWI6aeMNbtFWng'),
-(2, 'https://drive.google.com/uc?id=1jDH0x1PFedn4pt_C9SJ43ENzZV7_foyF'),
-(3, 'https://drive.google.com/uc?id=1-ZoN_CsbMJlseKCncLRUJzLaQXhOukdi'),
-(4, 'https://drive.google.com/uc?id=10-A2bGs7bU3NmhZmfkf0IIVJJmvcPfx9'),
-(5, 'https://drive.google.com/uc?id=1jEGHcPcH1qMmYFclD1K1tyuGjYzIXX_i'),
-(6, 'https://drive.google.com/uc?id=1NoiGmvU0F3hYGs4kFsuk25jHzJQu8lpR'),
-(7, 'https://drive.google.com/uc?id=1hHB9cdeu_UWh5cyn9rnYRGfCzKI_aSOp'),
-(8, 'https://drive.google.com/uc?id=1yfHeNQ2kMcg6s8Pql2Caee935oVRldIT')
 
 --  VIEW TABELA dProduto
---  Realizado Join com as tabelas de Categoria, Subcategoria e Imagem para obter o nome da categoria e subcategoria e URL com foto de cada categoria
 
 CREATE VIEW VwdProduto AS
   SELECT 
@@ -86,7 +94,7 @@ CREATE VIEW VwdProduto AS
 
   -- VIEW TABELA dLoja
 
-CREATE VIEW fVendas AS
+CREATE VIEW VwfVendas AS
  SELECT 
 	fVendas.DateKey AS 'DataVenda',
 	fVendas.StoreKey AS 'IdLoja',
@@ -97,4 +105,7 @@ CREATE VIEW fVendas AS
 	fVendas.ReturnQuantity AS 'QtdDevolvida'
 
  FROM FactOnlineSales AS fVendas
+ LEFT JOIN DimCustomer AS dCliente ON fVendas.CustomerKey = dCliente.CustomerKey
+ WHERE CONCAT(dCliente.FirstName, ' ', dCliente.MiddleName, ' ', dCliente.LastName) <> ''
+
 
